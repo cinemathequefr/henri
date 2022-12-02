100d742ea65d46fbc7736753f5c6a3dd72cf5099
NLTESOWN <nlte@fastmail.com>
2022-12-02 11:30:13 +0100

    Opération béton

--- docs/client/update_pick_status.b43cdceb.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./dayjs.min.49cf339f.js";import{l as t}from"./client.9f4ec259.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
