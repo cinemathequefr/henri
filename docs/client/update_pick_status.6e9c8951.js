c35793d32024364639abf858625993f4585997cc
NLTESOWN <nlte@fastmail.com>
2023-01-23 18:55:57 +0100

    Export with matomo trackingé

---
+++ docs/client/update_pick_status.6e9c8951.js
@@ -0,0 +1,1 @@
+import{d as s}from"./dayjs.min.0e94113c.js";import{l as t}from"./client.3df50207.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
