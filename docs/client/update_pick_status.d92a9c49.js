939c270b90322bad82da33acd87ac0e3a586c29e
NLTESOWN <nlte@fastmail.com>
2023-01-26 17:18:36 +0100

    Test with matomo

---
+++ docs/client/update_pick_status.d92a9c49.js
@@ -0,0 +1,1 @@
+import{d as s}from"./dayjs.min.ac1b84ce.js";import{l as t}from"./client.b3fcc41e.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
