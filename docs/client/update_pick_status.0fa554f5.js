ce2a9ecd7b0c5c246f6049f308b89fbb56e1d36e
NLTESOWN <nlte@fastmail.com>
2023-02-20 11:28:59 +0100

    U

--- docs/client/update_pick_status.0fa554f5.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./dayjs.min.3a401125.js";import{l as t}from"./client.5643450a.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
