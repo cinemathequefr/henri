b8ed5b9d8bc60830e05b33b9808bbb89ae074e41
NLTESOWN <nlte@fastmail.com>
2024-01-09 16:00:46 +0100

    La Sultane de l'amour

---
+++ docs/client/update_pick_status.20c842ab.js
@@ -0,0 +1,1 @@
+import{d as s}from"./dayjs.min.e99faf19.js";import{l as t}from"./client.be7cfbf6.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
