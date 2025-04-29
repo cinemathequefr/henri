bcfa79983d5c36f650f38622825c5bbcda0f7fa2
NLTESOWN <nlte@fastmail.com>
2025-04-29 15:39:05 +0200

    Le P'tit Parigot 1-2

---
+++ docs/client/update_pick_status.f5e1ced1.js
@@ -0,0 +1,1 @@
+import{d as s}from"./dayjs.min.fa7b6858.js";import{D as t}from"./client.f0fa7e4f.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
