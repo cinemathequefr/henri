051b20080a77b819a0265fcbeda233e3e5bbd95f
NLTESOWN <nlte@fastmail.com>
2026-01-06 16:07:28 +0100

    T'as de beaux escaliers

---
+++ docs/client/update_pick_status.e9d1efe8.js
@@ -0,0 +1,1 @@
+import{d as s}from"./dayjs.min.e99ebe5b.js";import{D as t}from"./client.ef091eaf.js";function a(a){return t(a).map((a=>t(a).assign({status:(1===a.status||2===a.status)&&a.dateUnpublished&&s().isAfter(a.dateUnpublished,"day")?3:a.status}).value())).filter((s=>1===s.status||2===s.status)).value()}export{a as u};
